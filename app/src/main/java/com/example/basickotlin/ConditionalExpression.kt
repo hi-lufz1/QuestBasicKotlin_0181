9bd961699c4933d2d3b5f8d2eac9482262e6a7a1
hi-lufz1 <latifufauzi@gmail.com>
2024-10-12 18:09:42 +0700

    perulangan

--- app/src/main/java/com/example/basickotlin/ConditionalExpression.kt
+++ app/src/main/java/com/example/basickotlin/ConditionalExpression.kt
@@ -7,7 +7,7 @@ package com.example.basickotlin
 fun ConditionalStatement()
 {
     println("=== Conditional Expression ===")
-}
+
 
 //If-Else
 val number = 10
@@ -30,6 +30,44 @@ when (day) {
 else -> println("Invalid day")
 }
 
+//Try-Catch
+val value = "10"
+try {
+    val result = value.toInt()
+    println("Result: $result")
+} catch (e: NumberFormatException) {
+    println("Invalid Number")
+}
+}
+
+fun  Perulangan(){
+    println()
+    println("=== Perulangan ===")
+    for ( i in 1..5){
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars){
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5){
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do{
+        println("Perulangan ke-$y")
+        y++
+    }while (y <= 5)
+
+}
+
+
 fun main(){
     ConditionalStatement()
+    Perulangan()
 }
