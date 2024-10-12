c1979a686ec71909e9ffe7a2b8e02f3036accaa1
hi-lufz1 <latifufauzi@gmail.com>
2024-10-12 17:58:47 +0700

    whenn

--- app/src/main/java/com/example/basickotlin/ConditionalExpression.kt
+++ app/src/main/java/com/example/basickotlin/ConditionalExpression.kt
@@ -11,8 +11,25 @@ fun ConditionalStatement()
 
 //If-Else
 val number = 10
-if (number > 0 ){
+if (number > 0 ) {
     println("Positive Number")
 } else {
     println("Negative Number")
+}
+
+//When
+val day = 1
+when (day) {
+1 -> println("Sunday")
+2 -> println("Monday")
+3 -> println("Tuesday")
+4 -> println("Wednesday")
+5 -> println("Thursday")
+6 -> println("Friday")
+7 -> println("Saturday")
+else -> println("Invalid day")
+}
+
+fun main(){
+    ConditionalStatement()
 }
